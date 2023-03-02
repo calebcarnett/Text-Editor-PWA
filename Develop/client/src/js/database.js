87aed24fdcc3b68e82c74dbf10417eed23510d2c
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-02 06:03:14 -0500

    updated commenting on put method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,11 +15,15 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
+  // Create a connection to the database database and version we want to use.
   const todosDb = await openDB("jate", 1);
   // Create a new transaction and specify the database and data privileges.
   const tx = todosDb.transaction("jate", "readwrite");
+  // Open up the desired object store.
   const store = tx.objectStore("jate");
+  // Use the .put() method to update the content in the database.
   const request = store.put({ content });
+  // Get confirmation of the request.
   const result = await request;
   console.log("🚀 - data saved to the database", result);
 };
