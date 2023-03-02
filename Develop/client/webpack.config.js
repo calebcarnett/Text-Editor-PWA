d018d2bbe1274eb1c5825245d04cbb6c4e2e16aa
calebcarnett <111102789+calebcarnett@users.noreply.github.com>
2023-03-02 07:18:56 -0500

    added webpackpwamanifest to create manifest.json file

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -21,12 +21,31 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "Text editor",
       }),
       // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
+      }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "Text editor!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
